842000ad3146532972e362bc5600f12d2ab1249f
JwuCode <jerrywu2390@gmail.com>
2023-03-09 14:16:06 -0500

    program can read structured text

--- main.py
+++ main.py
@@ -17,33 +17,39 @@ def displayImages(images):
     plt.show()
 
 def drawBoxes(img):
+    #Converts input to grayscale image, then applies nonlocal means algo to remove noise
     image = img
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
-    cnts = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    #thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+    nldenoise = cv.fastNlMeansDenoising(gray, None, 20, 7, 21) 
+    cnts = cv.findContours(nldenoise, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
     for c in cnts:
         x,y,w,h = cv.boundingRect(c)
         cv.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
     displayImages([image])
 
-#image = cv.imread('./testingImages/johnTest.jpeg')
-#image = cv.imread('./testingImages/test.jpg')
-#image = cv.imread('./testingImages/stocks.jpeg')
-image = cv.imread('./testingImages/ECETB.png')
+def createFile(text):
+    tFile = open("imageText.txt", "w")
+    tFile.write(text)
+    tFile.close()   
 
 
-gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
-cnts = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-for c in cnts:
-    x,y,w,h = cv.boundingRect(c)
-    cv.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
+def readFile():
+    #NOTE ADD FILE TYPE CHECKER FOR PDF 
 
-#dst = cv.GaussianBlur(image,(5,5),0)
-#image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-#dst = cv.fastNlMeansDenoisingColored(image,None,10,10,7,21)
-drawBoxes(image)
-#text = pytesseract.image_to_string(dst)
-#print(text)
+
+    #image = cv.imread('./testingImages/johnTest.jpeg')
+    #image = cv.imread('./testingImages/test.jpg')
+    #image = cv.imread('./testingImages/stocks.jpeg')
+    image = cv.imread('./testingImages/ECETB.png')
+
+
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+    #dst = cv.GaussianBlur(image,(5,5),0)
+    #image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    #dst = cv.fastNlMeansDenoisingColored(image,None,10,10,7,21)
+    #drawBoxes(image)
+    text = pytesseract.image_to_string(image)
+    createFile(text)
